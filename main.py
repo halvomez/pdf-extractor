5ffca4ef8bf73684c6b1250a92402d2e96c719ae
Kharitonov Aleksei <haritonov_aa@srvhub.ru>
2020-04-05 15:33:23 +0300

    added flat convertion

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+import uuid
 import logging
 import argparse
 import fitz
+from time import time
 from pathlib import Path
 from multiprocessing import Pool
 
@@ -13,9 +15,16 @@ parser = argparse.ArgumentParser()
 parser.add_argument('input_dir', help='specify input dir path for convert')
 parser.add_argument('-out', '--output_dir',
                     help='specify output dir path for save jpg')
+parser.add_argument('-f', '--flat', action='store_true',
+                    help='convert to output_dir without dirs hierarchy')
 parser.add_argument('-q', '--quality', type=float, default=2.5,
                     help='specify convert quality')
 args = parser.parse_args()
+
+
+class PDFWorkerError(Exception):
+    def __init__(self):
+        self.message = 'can\'t load images, check input dir'
 
 
 def collect_pdf_for_convert(input_dir):
@@ -25,10 +34,34 @@ def collect_pdf_for_convert(input_dir):
             if file.split('.')[-1].lower() == 'pdf':
                 _pdfs.append(os.path.join(root, file))
 
+    if not _pdfs:
+        raise PDFWorkerError()
+
     return _pdfs
 
 
-def saving_separate_pages(file):
+def saving_img_pages(file, target_path, postfix=None):
+    fname = str(Path(file).stem)
+    with open(file, 'rb') as file_:
+        buf = file_.read()
+        postfix_ = uuid.uuid4().time_low
+
+        with fitz.Document(fname, buf) as doc:
+            new_fname = f'{fname}_{postfix_}' if postfix else fname
+            if doc.pageCount > 1:
+                for page in doc:
+                    mat = fitz.Matrix(args.quality, args.quality)
+                    pix = page.getPixmap(matrix=mat, alpha=False)
+                    pix.writeImage(
+                        os.path.join(target_path,
+                                     f'{new_fname}-page{page.number + 1}.jpg'))
+
+            else:
+                pix = doc[0].getPixmap()
+                pix.writeImage(os.path.join(target_path, f'{new_fname}.jpg'))
+
+
+def convert_with_hierarchy(file):
     logger.info(f' converting {file}')
     processing_path = str(Path(file).parent)
 
@@ -41,31 +74,37 @@ def saving_separate_pages(file):
         target_path = os.path.join(args.output_dir,
                                    *fpath_parts[len(ipath_parts):-1])
 
-    if os.path.exists(processing_path):
-        os.makedirs(processing_path, exist_ok=True)
+    if not os.path.exists(target_path):
+        os.makedirs(target_path, exist_ok=True)
 
-    fname = str(Path(file).stem)
+    saving_img_pages(file, target_path)
+
+
+def convert_without_hierarchy(file):
+    logger.info(f' converting {file}')
+
+    if not args.output_dir:
+        target_path = os.path.join(*Path(file).parts[:2])
+    else:
+        target_path = args.output_dir
 
     if not os.path.exists(target_path):
         os.makedirs(target_path, exist_ok=True)
 
-    with open(file, 'rb') as file_:
-        with fitz.Document(fname, file_.read()) as doc:
-            if doc.pageCount > 1:
-                for page in doc:
-                    mat = fitz.Matrix(args.quality, args.quality)
-                    pix = page.getPixmap(matrix=mat, alpha=False)
-                    pix.writeImage(
-                        os.path.join(target_path,
-                                     f'{fname}-page{page.number + 1}.jpg'))
-
-            else:
-                pix = doc[0].getPixmap()
-                pix.writeImage(os.path.join(target_path, f'{fname}.jpg'))
+    saving_img_pages(file, target_path, postfix=True)
 
 
 if __name__ == '__main__':
-    pdfs = collect_pdf_for_convert(args.input_dir)
-    with Pool(processes=None) as pool:
-        pool.map(saving_separate_pages, pdfs)
-    logger.info('all tasks is done')
+    start = time()
+    try:
+        pdfs = collect_pdf_for_convert(args.input_dir)
+    except PDFWorkerError as e:
+        logging.error(e.message)
+    else:
+        with Pool(processes=None) as pool:
+            if args.flat:
+                pool.map(convert_without_hierarchy, pdfs)
+            else:
+                pool.map(convert_with_hierarchy, pdfs)
+        logger.info('all tasks is done')
+        logger.info(f'elapsed time is {int((time() - start) * 100) / 100} sec')
